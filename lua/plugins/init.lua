e51396cb94782b0107015b178d4464dc9f552b71
Rafael Barreto <barretor@delagelanden.com>
2021-08-25 08:15:37 -0300

    change all neovim windows config for lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,15 +1,15 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 -- Only required if you have packer configured as `opt`
-require('plugins.bufferLine')
-require('plugins.statusline')
-require('plugins.Lsp')
+-- require('plugins.bufferLine')
+-- require('plugins.statusline')
+-- require('plugins.Lsp')
 
 vim.cmd [[packadd packer.nvim]]
 
 -- -- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
 -- vim._update_package_paths()
 
-return require('packer').startup(function()
+local startup = require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
@@ -22,9 +22,11 @@ return require('packer').startup(function()
   -- using packer.nvim
   use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'kabouzeid/nvim-lspinstall'
+  -- Coc
+  use {
+    'neoclide/coc.nvim',
+    branch = 'release'
+  }
 
   -- galaxyline
   use {
@@ -42,3 +44,10 @@ return require('packer').startup(function()
   }
 
 end)
+
+-- import modules
+require('plugins.bufferLine')
+require('plugins.statusline')
+require('plugin.Coc')
+
+return startup
